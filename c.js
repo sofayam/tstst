971119b5c8ecf44c0aa186e83fef2bd569e388e3
Mark Andrew <mark.andrew@gmail.com>
2016-06-08 21:11:51 +0200

    string fun

--- c.js
+++ c.js
@@ -1,5 +1,7 @@
 "use strict";
 var b_1 = require("./b");
 var f = new b_1.default(2, 4);
-console.log(f.talk("about x  y myself"));
+console.log(f.talk("about x  y \
+myself"));
+console.log("this is the sum " + f.sum() + " which is all");
 //# sourceMappingURL=c.js.map
--- c.ts
+++ c.ts
@@ -2,4 +2,7 @@ import Foo from "./b"
 
 var f = new Foo(2,4);
 
-console.log(f.talk("about x  y myself"));
+console.log(f.talk("about x  y \
+myself"));
+
+console.log(`this is the sum ${f.sum()} which is all`)
